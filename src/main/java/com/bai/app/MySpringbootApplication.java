bc96eddc9b3376113c6b2692abea8935ec4809fb
byebai95 <15217742393@163.com>
2021-09-03 14:28:02 +0800

    update

--- src/main/java/com/bai/app/MySpringbootApplication.java
+++ src/main/java/com/bai/app/MySpringbootApplication.java
@@ -1,7 +1,9 @@
 package com.bai.app;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan(basePackages = {"com.bai.app.dao"})
@@ -9,7 +11,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MySpringbootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MySpringbootApplication.class, args);
+        SpringApplication application = new SpringApplication(MySpringbootApplication.class);
+        //设置 banner 输出模式 ，默认为 console
+        application.setBannerMode(Banner.Mode.OFF);
+
+        application.setWebApplicationType(WebApplicationType.SERVLET);
+
+        //日志是否打印 app 启动耗时, 默认开启
+        application.setLogStartupInfo(true);
+
+        //System.setProperty("spring.boot.enableautoconfiguration","false");
+
+        application.run(args);
+
     }
 
 }
--- src/main/java/com/bai/app/web/UserController.java
+++ src/main/java/com/bai/app/web/UserController.java
@@ -21,4 +21,9 @@ public class UserController {
     public User getUserInfoById(@RequestParam("uid")Long uid){
         return userService.getUserInfoById(uid);
     }
+
+    @GetMapping
+    public String getMessage(){
+        return "Hello,World";
+    }
 }
